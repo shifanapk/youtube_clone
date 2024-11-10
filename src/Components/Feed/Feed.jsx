c57450574c0647588c2e1e9b322562935829acb2
shifanapk <shifanapk200@gmail.com>
2024-11-10 21:44:41 +0530

    changes

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,135 @@
+import React from 'react'
+import './Feed.css'
+import thumbnail1 from '../../assets/thumbnail1.png'
+import thumbnail2 from '../../assets/thumbnail2.png'
+import thumbnail3 from '../../assets/thumbnail3.png'
+import thumbnail4 from '../../assets/thumbnail4.png'
+import thumbnail5 from '../../assets/thumbnail5.png'
+import thumbnail6 from '../../assets/thumbnail6.png'
+import thumbnail7 from '../../assets/thumbnail7.png'
+import thumbnail8 from '../../assets/thumbnail8.png'
+import thumbnail31 from '../../assets/thumbnail31.png'
+
+
+const Feed = () => {
+    return (
+        <div className='feed'>
+            <div className='card'>
+                <img src={thumbnail1} alt='' />
+                <h2>Best channel to learn coding that help you to be a web developer</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail2} alt='' />
+                <h2>Master JavaScript in 30 days with interactive tutorials</h2>
+                <h3>CodeAcademy</h3>
+                <p>10k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail3} alt='' />
+                <h2>React basics for beginners: From zero to hero</h2>
+                <h3>TechSavvy</h3>
+                <p>40k views &bull; 40 min ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail4} alt='' />
+                <h2>CSS Tricks to make your website stand out</h2>
+                <h3>DesignLab</h3>
+                <p>100k views &bull; 1 day ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail5} alt='' />
+                <h2>Understanding APIs and how they work in real projects</h2>
+                <h3>DevMastery</h3>
+                <p>15k views &bull; 5 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail6} alt='' />
+                <h2>Top 10 VS Code extensions for developers</h2>
+                <h3>CodeHub</h3>
+                <p>19k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail7} alt='' />
+                <h2>Node.js for backend development: A complete guide</h2>
+                <h3>BackendPro</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail8} alt='' />
+                <h2>Building a full-stack application with MERN stack</h2>
+                <h3>FullstackSchool</h3>
+                <p>11k views &bull; 7 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail1} alt='' />
+                <h2>How to ace your coding interviews with confidence</h2>
+                <h3>CareerPro</h3>
+                <p>105k views &bull; 5 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail2} alt='' />
+                <h2>Mastering CSS Grid and Flexbox for responsive design</h2>
+                <h3>GreatStack</h3>
+                <p>155k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail3} alt='' />
+                <h2>Top JavaScript frameworks to learn in 2024</h2>
+                <h3>Simplilearn</h3>
+                <p>166k views &bull; 3 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail4} alt='' />
+                <h2>SEO tips for web developers to boost site ranking</h2>
+                <h3>WebSeo</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail5} alt='' />
+                <h2>Getting started with TypeScript: Why and how to use it</h2>
+                <h3>CodeInsight</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail6} alt='' />
+                <h2>Understanding Git and GitHub for collaborative development</h2>
+                <h3>GitMaster</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail31} alt='' />
+                <h2>Karuppinazhaku|HipHop Dance Cover|Lakshmi P Mahesh</h2>
+                <h3>Lakshmi P Mahesh</h3>
+                <p>9.1k views &bull; 3 years ago</p>
+            </div>
+
+            <div className='card'>
+                <img src={thumbnail8} alt='' />
+                <h2>Best channel to learn coding that help you to be a web developer</h2>
+                <h3>GreatStack</h3>
+                <p>15k views &bull; 2 days ago</p>
+            </div>
+
+
+        </div>
+
+    )
+}
+
+export default Feed
